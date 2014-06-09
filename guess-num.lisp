6fc0d4507035ce381b9dbfb2a9e3029440905528
Break Yang <BreakDS@gmail.com>
2014-06-09 08:11:42 -0400

    add game over condition

--- guess-num.lisp
+++ guess-num.lisp
@@ -41,14 +41,18 @@
 		(cons "number" num)
 		(cons "hint" (format nil "~aA~aB" a b)))
 	  (hunchentoot:session-value 'records))
-    (reverse (hunchentoot:session-value 'records))))
+    `(:obj ,(cons "records" (reverse (hunchentoot:session-value 'records)))
+           ,(cons "guessed" (if (= a 4) t :f)))))
 
 (def-widget game-box ()
-    ((state (records (array)))
+    ((state (records (array))
+            (game-over nil))
      (handle-submit (data)
                     (with-rpc (calculate-hint (@ data text))
 		      (chain this (set-state (create records 
-						     rpc-result)))
+						     (@ rpc-result records)
+                                                     game-over (@ rpc-result
+                                                                  guessed))))
                       (chain console (log rpc-result)))
 		    false))
   #jsx(:div ()
@@ -56,12 +60,14 @@
 	    (:div ((class-name "topcoat-list__container"))
 		  (:ul ((class-name "topcoat-list"))
 		       (:h3 ((class-name "topcoat-list__header"))
-			    "Result:")
+			    "Game Records")
 		       (chain (local-state records) 
 			      (map (lambda (record)
 				     (:game-record ((number (@ record number))
 						    (hint (@ record hint)))))))))
-            (:interaction-area ((process-guess (@ this handle-submit))))))
+            (if (local-state game-over)
+                (:reset-panel)
+                (:interaction-area ((process-guess (@ this handle-submit)))))))
 
 (def-widget interaction-area (process-guess)
     ((handle-submit ()
@@ -78,6 +84,14 @@
              (:button ((class-name "topcoat-button--cta")
                        (type "submit")) "Guess")))
 
+(def-widget reset-panel ()
+    ((handle-submit () 
+                    (chain window location (reload t))))
+  #jsx(:form ((on-submit (@ this handle-submit)))
+             (:button ((class-name "topcoat-button--cta")
+                       (style (create "background-color" "lightgreen")))
+                      "You win. Restart.")))
+
 (def-widget game-record (number hint)
     ()
   #jsx(:li ((class-name "topcoat-list__item"))
@@ -87,12 +101,9 @@
 		  hint)))
 
 (def-realispic-app (guess-num-app :title "Guess Number"
-				  ;; :libs ("http://fb.me/react-0.10.0.js"
-				  ;; 	 "http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js")
-                                  ;; :css ("http://cdnjs.cloudflare.com/ajax/libs/topcoat/0.8.0/css/topcoat-mobile-light.min.css")
-				  :libs ("/react-0.10.0.js"
-					 "/jquery.js")
-				  :css ("/topcoat-mobile-light.css")
+				  :libs ("http://fb.me/react-0.10.0.js"
+				  	 "http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js")
+                                  :css ("http://cdnjs.cloudflare.com/ajax/libs/topcoat/0.8.0/css/topcoat-mobile-light.min.css")
 				  :document-base (asdf:system-source-directory 'guess-num)
                                   :port 14386)
   (with-rpc (initialize)
